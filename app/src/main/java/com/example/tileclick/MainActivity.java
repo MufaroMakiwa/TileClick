cf643d4bea3ab08afed4db16f0ea775d9324f3fd
Mufaro Makiwa <mufaromakiwa@dhcp-18-20-203-59.dyn.mit.edu>
2020-03-14 18:28:59 -0400

    Scroll view implemented

--- app/src/main/java/com/example/tileclick/MainActivity.java
+++ app/src/main/java/com/example/tileclick/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.tileclick;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button gameplay, aboutme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpUIViews();
+
+        gameplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GamePlay.class);
+                startActivity(intent);
+            }
+        });
+
+        aboutme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, About.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    public void setUpUIViews() {
+        gameplay = findViewById(R.id.game);
+        aboutme = findViewById((R.id.aboutme));
+    }
+
+
 }
+
