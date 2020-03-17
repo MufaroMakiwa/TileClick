59ea8c86c3e628e580fa37e7594ae6017b70371c
Mufaro Makiwa <mufaromakiwa@Mufaros-MacBook-Pro.local>
2020-03-17 15:53:01 -0400

    Toast added

--- app/src/main/java/com/example/tileclick/About.java
+++ app/src/main/java/com/example/tileclick/About.java
@@ -3,6 +3,8 @@ package com.example.tileclick;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class About extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toastmessage, Toast.LENGTH_LONG);
+        toast.show();
     }
 }
